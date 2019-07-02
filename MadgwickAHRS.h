2b9fa81de87477b5df0a056a23204ab9be7151ce
ErikBob <erik@boback.org>
2019-07-02 17:25:34 +0200

    Add files via upload

---
+++ MadgwickAHRS.h
@@ -0,0 +1,45 @@
+//=====================================================================================================
+// MadgwickAHRS.h
+//=====================================================================================================
+//
+// Implementation of Madgwick's IMU and AHRS algorithms.
+// See: http://www.x-io.co.uk/node/8#open_source_ahrs_and_imu_algorithms
+//
+// Date			Author          Notes
+// 29/09/2011	SOH Madgwick    Initial release
+// 02/10/2011	SOH Madgwick	Optimised for reduced CPU load
+//
+// 05/06/2019   Erik Boback     adapted to CPP // all rights remain with the previous author 
+//=====================================================================================================
+#ifndef MadgwickAHRS_h
+#define MadgwickAHRS_h
+
+class MadgwickAHRS
+{
+private:
+    //----------------------------------------------------------------------------------------------------
+    // Variable declaration
+
+    float beta;				// algorithm gain
+    float sampleFreq;
+    float q0, q1, q2, q3;	// quaternion of sensor frame relative to auxiliary frame
+
+    //---------------------------------------------------------------------------------------------------
+public:
+    //----------------------------------------------------------------------------------------------------
+    // Function declarations
+    MadgwickAHRS();
+    MadgwickAHRS(float inputBeta, float inputSampleFreq);
+ 
+    void update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
+    void updateIMU(float gx, float gy, float gz, float ax, float ay, float az);
+
+    void getQuaternion(float* dest);
+
+    //---------------------------------------------------------------------------------------------------
+};
+
+#endif
+//=====================================================================================================
+// End of file
+//=====================================================================================================
